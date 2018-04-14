4447f33aaba2d3bc9a34d85bf0e7418cf1ba5894
abhi4578 <29863835+abhi4578@users.noreply.github.com>
2018-04-14 23:03:06 +0530

    Update client2.c

--- client/client2.c
+++ client/client2.c
@@ -1,17 +1,20 @@
+#include <signal.h>
 #include <stdio.h>
 #include <sys/socket.h>
 #include <stdlib.h>
 #include <netinet/in.h>
 #include <sys/types.h>
 #include <string.h>
-#include <unistd.h>
 #include <fcntl.h>
+#include <sys/wait.h>
 #include <unistd.h>
+#include <pthread.h>
 #include <arpa/inet.h>
+
 #define PORT 1250
 int main(int argc,char **argv)
-{
-    int client_fd, new_socket, valread;
+{   pid_t pid;
+    int client_fd, new_socket, valread,file;
     struct sockaddr_in address;
     int addrlen = sizeof(address);
      char filename[80],recvi[80],buffer[80];
@@ -30,24 +33,32 @@ int main(int argc,char **argv)
     address.sin_port = htons( PORT );
     inet_pton(AF_INET,argv[1],&address.sin_addr);
     connect(client_fd,(struct sockaddr*)& address,addrlen);
-    printf("enter filename\n");
-    scanf("%s",filename);   
-    FILE *Compile_d=fopen(filename,"r");
+
+    if((pid=fork())==0)
+    { file=creat("add.c",0666);
+      execlp("/usr/bin/vi","vi","add.c",NULL);         //vi editor
+    }
+    else
+        { wait(NULL);
+        //sleep(2);
+    }
+
+            
+    FILE *Compile_d=fopen("add.c","r");
     if(Compile_d==NULL)
         perror("file doesn't exist");
     
-    write(client_fd,filename,80);
-     
-    
-
+   
+    bzero(&buffer,sizeof(buffer));
     while(fgets(buffer,80,Compile_d)!=NULL)       // code submission
-        { // puts(buffer);
-            write(client_fd,buffer,sizeof(buffer));   
+        { //puts(buffer);
+            write(client_fd,buffer,sizeof(buffer)); 
+            bzero(&buffer,sizeof(buffer))  ;
         }
-        write(client_fd,"$",1);
-        while(read(client_fd,recvi,80)!=0)
+        write(client_fd,"$",1);         
+        while(read(client_fd,recvi,80)!=0)              // recieve message-compile error/correct answer/wrong answer
            {printf("%s",recvi);
-        }   
+             bzero(&recvi,sizeof(recvi));}   
 
     close(client_fd);
 }
